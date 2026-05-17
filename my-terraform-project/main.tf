04c1781d43a0916331a8f0e56fbcdf5096c4a2f3
Abhishek <stoicorion22@gmail.com>
2026-05-17 10:35:49 +0530

    change asg health check to 600

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -347,7 +347,7 @@ resource "aws_autoscaling_group" "aws_asg" {
   target_group_arns = [aws_lb_target_group.tg.arn]
 
   health_check_type         = "ELB"
-  health_check_grace_period = 300
+  health_check_grace_period = 600
 
   instance_refresh {
     strategy = "Rolling"
