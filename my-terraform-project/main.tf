1743a91e3e41ff653f50e9cefbe72072c180c9dd
Abhishek <stoicorion22@gmail.com>
2026-05-22 11:53:15 +0530

    Added NodePort security group rule

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -154,6 +154,14 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Allows deployment commands to reach K3s
   }
 
+  ingress {
+    description = "Kubernetes NodePort"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
